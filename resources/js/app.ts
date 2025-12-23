0cc05e110b217ee1dafe8db3c688fc083128c4d4
Usama Munir <hello@usamamuneer.me>
2025-12-23 13:21:01 +0000

    added reverb config

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,10 +1,16 @@
 import '../css/app.css';
 
 import { createInertiaApp } from '@inertiajs/vue3';
+import { configureEcho } from '@laravel/echo-vue';
+import Echo from 'laravel-echo';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
 import { initializeTheme } from './composables/useAppearance';
+
+configureEcho({
+    broadcaster: 'reverb',
+});
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -27,3 +33,31 @@ createInertiaApp({
 
 // This will set light / dark mode on page load...
 initializeTheme();
+
+if (typeof window !== 'undefined') {
+
+    const reverbHost = import.meta.env.VITE_REVERB_HOST;
+    const reverbPort = import.meta.env.VITE_REVERB_PORT;
+    const reverbScheme = import.meta.env.VITE_REVERB_SCHEME;
+    const reverbKey = import.meta.env.VITE_REVERB_APP_KEY;
+
+    window.Echo = new Echo({
+        broadcaster: 'reverb',
+        key: reverbKey,
+        wsHost: reverbHost,
+        wsPort: reverbPort,
+        wssPort: reverbPort,
+        forceTLS: reverbScheme === 'https',
+        enabledTransports: ['ws', 'wss'],
+        disableStats: true,
+        authEndpoint: '/broadcasting/auth',
+        auth: {
+            headers: {
+                'X-CSRF-TOKEN':
+                    document
+                        .querySelector('meta[name="csrf-token"]')
+                        ?.getAttribute('content') || '',
+            },
+        },
+    });
+}
